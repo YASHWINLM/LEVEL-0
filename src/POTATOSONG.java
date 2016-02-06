2edb7380620b5cd28d51e9209510b188da4be557
Shiva Malhotra <Shiva.malhotra.email@gmail.com>
2016-02-05 18:35:58 -0800

    puzzles

---
+++ src/POTATOSONG.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class POTATOSONG {
+	public static void main(String[] args) {
+		for (int i = 1; i < 9; i++) {
+		if (i==4) {
+			JOptionPane.showMessageDialog(null, "4");
+			
+		}
+		else if (i==8){
+			JOptionPane.showMessageDialog(null, "MORE!!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null,i + " POTATO");
+		}
+		}
+		
+		}
+	}
+
+
